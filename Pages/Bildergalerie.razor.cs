8dc19508714bb8f5f4b8ae30cd041f54f43d12df
Leon <leon.jost7@gmail.com>
2022-05-07 20:04:10 +0200

    test

--- Pages/Bildergalerie.razor.cs
+++ Pages/Bildergalerie.razor.cs
@@ -8,7 +8,7 @@ namespace m152_bildergalerie.Pages
         private bool _arrows = true;
         private bool _bullets = true;
         private bool _autocycle = true;
-        private IList<string> _source = new List<string>() { "images/tes6_teaser.png", "images/the-elder-scrolls-2560x1440.jpg", "images/whiterun_landscape.png" };
+        private IList<string> _source = new List<string>() { "images/riften_pier.jpg", "images/solitude_landscape.jpg", "images/whiterun_castle.jpg" };
         private int selectedIndex = 2;
     }
 }
