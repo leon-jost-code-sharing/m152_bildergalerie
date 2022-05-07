3f9cc60d64fd2b6d318278f7db0c6254cfc8dc89
Leon <leon.jost7@gmail.com>
2022-05-07 19:30:13 +0200

    sync

---
+++ Pages/Bildergalerie.razor.cs
@@ -0,0 +1,14 @@
+﻿using MudBlazor;
+
+namespace m152_bildergalerie.Pages
+{
+    public partial class Bildergalerie
+    {
+        private MudCarousel<string> _carousel;
+        private bool _arrows = true;
+        private bool _bullets = true;
+        private bool _autocycle = true;
+        private IList<string> _source = new List<string>() { "images/tes6_teaser.png", "images/the-elder-scrolls-2560x1440.jpg", "images/whiterun_landscape.png" };
+        private int selectedIndex = 2;
+    }
+}
